57ce07aeb9ed68d21d1bf2310024a35a714363ea
matthew <matthew@matthews-MacBook-Pro.local>
2015-08-12 20:23:05 -0400

    built out homepage based on wire

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,6 +13,8 @@
 
 Route::get('/', 'WelcomeController@index');
 
+Route::get('solutions-home', 'SolutionsController@index');
+
 Route::get('home', 'HomeController@index');
 
 Route::controllers([
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,7 @@
 	<title>Laravel</title>
 
 	<link href="/css/app.css" rel="stylesheet">
+	<link href="/css/custom.css" rel="stylesheet">
 
 	<!-- Fonts -->
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
@@ -28,32 +29,33 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#">Laravel</a>
-			</div>
-
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav">
-					<li><a href="/">Home</a></li>
-				</ul>
-
-				<ul class="nav navbar-nav navbar-right">
-					@if (Auth::guest())
-						<li><a href="/auth/login">Login</a></li>
-						<li><a href="/auth/register">Register</a></li>
-					@else
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href="/auth/logout">Logout</a></li>
-							</ul>
-						</li>
-					@endif
-				</ul>
+				<a class="navbar-brand" href="#">Solutions Homepage Headline</a>
 			</div>
 		</div>
 	</nav>
 
 	@yield('content')
+
+	<div id="footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-4">
+					<img src="http://www.placehold.it/50x50" class="" style="margin-top:15px">
+				</div>
+				<div class="col-xs-4">
+					
+				</div>
+				<div class="col-xs-4">
+					<span style="margin-top:35px">
+						<a href="" class="btn btn-primary btn-lg" style="margin-top:15px">Solutions</a>
+							&nbsp;
+						<a href="" class="btn btn-primary btn-lg" style="margin-top:15px">Zone Map</a>
+					</span>
+				</div>
+			</div>
+		</div>
+			
+	</div>
 
 	<!-- Scripts -->
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
